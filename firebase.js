1455e7887b37577f4f4ec966ef0c817e1fbaa778
e-rine <elmedalla@up.edu.ph>
2026-05-28 10:18:51 +0800

    merge push

--- firebase.js
+++ firebase.js
@@ -1,10 +1,11 @@
-
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+  import { initializeApp }  from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  import { getAnalytics }   from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
+  import { getFirestore }   from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+  import { getAuth }        from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
+
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -23,4 +24,5 @@
   const analytics = getAnalytics(app);
 
   //Initialize firebase products to use
-  export const db = getFirestore(app)
+  export const db   = getFirestore(app);
+  export const auth = getAuth(app);
